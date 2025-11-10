0929519a95f1b0700835bbbf5f320bd5237e61bf
Mohamed Rafiq I <143062657+Boxcode90@users.noreply.github.com>
2025-11-10 07:57:45 +0530

    Update server.js

--- src/Server/server.js
+++ src/Server/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://willowy-gecko-e4f90a.netlify.app/",
+    origin: "https:/quizappnm.netlify.app/",
     credentials: true,
   })
 );
