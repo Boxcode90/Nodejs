110710c6b9327f00d32154ba3ac115890cfba64f
Mohamed Rafiq I <143062657+Boxcode90@users.noreply.github.com>
2025-11-10 08:04:46 +0530

    Update server.js

--- src/Server/server.js
+++ src/Server/server.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https:/quizappnm.netlify.app",
+    origin: "https://quizappnm.netlify.app",
     credentials: true,
   })
 );
